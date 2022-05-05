8fcbb21701d386a451f662d19463c87abf4a309b
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-05-06 00:59:21 +0300

    Add home screen scene and style

--- src/main/java/com/readile/readile/views/HomeScreen.java
+++ src/main/java/com/readile/readile/views/HomeScreen.java
@@ -1,7 +1,11 @@
 package com.readile.readile.views;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomeScreen extends Application {
     public static void main(String[] args) {
@@ -9,7 +13,10 @@ public class HomeScreen extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
