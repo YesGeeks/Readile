ad4a91e365a026c1055b959e2930ced161f53b17
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-05-07 20:01:39 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -17,13 +17,17 @@ module com.readile.readile {
     requires spring.beans;
     requires com.jfoenix;
     requires spring.boot.autoconfigure;
+    requires org.slf4j;
 
     opens com.readile.readile;
 
-    opens com.readile.readile.views to javafx.fxml;
+    opens com.readile.readile.views to javafx.fxml, spring.core;
     opens com.readile.readile.controllers to javafx.fxml;
-    opens com.readile.readile.models.userbook to org.hibernate.orm.core;
-    opens com.readile.readile.models.book to org.hibernate.orm.core;
-    opens com.readile.readile.models.user to org.hibernate.orm.core;
+    opens com.readile.readile.models.userbook to org.hibernate.orm.core, spring.core;
+    opens com.readile.readile.models.book to org.hibernate.orm.core, spring.core;
+    opens com.readile.readile.models.user to org.hibernate.orm.core, spring.core;
     exports com.readile.readile.views;
+    exports com.readile.readile.controllers to spring.beans;
+    exports com.readile.readile.services.implementation to spring.beans;
+    opens com.readile.readile.services.implementation to spring.core;
 }
