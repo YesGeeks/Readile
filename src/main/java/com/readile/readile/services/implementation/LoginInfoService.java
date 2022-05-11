9e3cac2c720fe894061be23ea971a26d45f2eb29
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-05-11 05:04:42 +0300

    Create LoginInfo service with authenticate method

---
+++ src/main/java/com/readile/readile/services/implementation/LoginInfoService.java
@@ -0,0 +1,85 @@
+package com.readile.readile.services.implementation;
+
+import com.readile.readile.models.user.LoginInfo;
+import com.readile.readile.models.user.User;
+import com.readile.readile.repositories.LoginInfoRepository;
+import com.readile.readile.services.CrudService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+@Service
+public class LoginInfoService implements CrudService<LoginInfo> {
+    @Autowired
+    private LoginInfoRepository loginInfoRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public LoginInfo save(LoginInfo entity) {
+        return loginInfoRepository.save(entity);
+    }
+
+    @Override
+    public LoginInfo update(LoginInfo entity) {
+        return loginInfoRepository.save(entity);
+    }
+
+    @Override
+    public void delete(LoginInfo entity) {
+        loginInfoRepository.delete(entity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        loginInfoRepository.deleteById(id);
+    }
+
+    @Override
+    public void deleteInBatch(List<LoginInfo> entities) {
+        loginInfoRepository.deleteAllInBatch(entities);
+    }
+
+    @Override
+    public LoginInfo findById(Long id) {
+        return loginInfoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<LoginInfo> findAll() {
+        return loginInfoRepository.findAll();
+    }
+
+    public LoginInfo findByUser(User user) {
+        return loginInfoRepository.findByUser(user);
+    }
+
+    public Boolean authenticate(String email, String password) {
+        User user = userService.findByEmail(email);
+        if (user == null) {
+            return false;
+        } else {
+            return getMD5(password).equals(findByUser(user).getPassword());
+        }
+    }
+
+    private String getMD5(String password) {
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md5.digest(password.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            StringBuilder hashText = new StringBuilder(no.toString(16));
+            while (hashText.length() < 32) {
+                hashText.insert(0, "0");
+            }
+            return hashText.toString();
+        } catch (NoSuchAlgorithmException exception) {
+            throw new RuntimeException();
+        }
+    }
+}
