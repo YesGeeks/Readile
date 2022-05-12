72452de7b32ac7f3c202b85b7a217e21dde50114
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-05-12 22:11:53 +0300

    Change getBookCard parameters type to UserBook

--- src/main/java/com/readile/readile/views/components/BookCard.java
+++ src/main/java/com/readile/readile/views/components/BookCard.java
@@ -1,49 +1,51 @@
 package com.readile.readile.views.components;
 
 import com.readile.readile.controllers.BookCardController;
+import com.readile.readile.models.userbook.UserBook;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
+import com.readile.readile.models.userbook.Rating;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 
 public class BookCard {
-    public Pane getBookCard(String bookName, int progress, String coverPath, String status, String stars) throws IOException {
+    public Pane getBookCard(UserBook userBook) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/BookCard.fxml"));
         Pane root = fxmlLoader.load();
         BookCardController controller = fxmlLoader.getController();
 
-        controller.bookName.setText(bookName);
-        controller.progress.setProgress(progress/100.0);
-        String path = "\"" + coverPath + "\"";
+        controller.bookName.setText(userBook.getBook().getName());
+        controller.progress.setProgress(Double.parseDouble(String.format("%.2f", (userBook.getCurrentPage()/userBook.getBook().getLength().doubleValue()))));
+        String path = "\"" + userBook.getBook().getCoverId() + "\"";
         controller.bookCover.setStyle("-fx-background-image: url(" + path + ");");
-        String statue = status.replace('_', ' ').toLowerCase();
+        String statue = String.valueOf(userBook.getStatus()).replace('_', ' ').toLowerCase();
         controller.status.setText(StringUtils.capitalize(statue));
-        setRating(stars, controller);
+        setRating(userBook.getRating(), controller);
 
         return root;
     }
 
-    private void setRating(String stars, BookCardController controller) {
+    private void setRating(Rating rating, BookCardController controller) {
         String path = String.valueOf(getClass().getResource("/icons/on.png"));
-        switch (stars) {
-            case "ONE_STAR" -> controller.star1.setStyle("-fx-image: url(" + path + ")");
-            case "TWO_STARS" -> {
+        switch (rating) {
+            case ONE_STAR -> controller.star1.setStyle("-fx-image: url(" + path + ")");
+            case TWO_STARS -> {
                 controller.star1.setStyle("-fx-image: url(" + path + ")");
                 controller.star2.setStyle("-fx-image: url(" + path + ")");
             }
-            case "THREE_STARS" -> {
+            case THREE_STARS -> {
                 controller.star1.setStyle("-fx-image: url(" + path + ")");
                 controller.star2.setStyle("-fx-image: url(" + path + ")");
                 controller.star3.setStyle("-fx-image: url(" + path + ")");
             }
-            case "FOUR_STARS" -> {
+            case FOUR_STARS -> {
                 controller.star1.setStyle("-fx-image: url(" + path + ")");
                 controller.star2.setStyle("-fx-image: url(" + path + ")");
                 controller.star3.setStyle("-fx-image: url(" + path + ")");
                 controller.star4.setStyle("-fx-image: url(" + path + ")");
             }
-            case "FIVE_STARS" -> {
+            case FIVE_STARS -> {
                 controller.star1.setStyle("-fx-image: url(" + path + ")");
                 controller.star2.setStyle("-fx-image: url(" + path + ")");
                 controller.star3.setStyle("-fx-image: url(" + path + ")");
