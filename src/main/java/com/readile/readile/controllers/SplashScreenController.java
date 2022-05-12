6a301fc837639737099d1a7b372ba84529d0d3ed
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-05-12 15:11:38 +0300

    Add thread to SplashScreenController

--- src/main/java/com/readile/readile/controllers/SplashScreenController.java
+++ src/main/java/com/readile/readile/controllers/SplashScreenController.java
@@ -1,16 +1,33 @@
 package com.readile.readile.controllers;
 
+import com.readile.readile.config.FxController;
+import com.readile.readile.views.StageManager;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Controller;
 
-public class SplashScreenController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Controller
+@FxmlView("/fxml/Splash.fxml")
+public class SplashScreenController implements Initializable, FxController {
     @FXML
     public HBox toolBar;
     private double xOffset = 0, yOffset = 0;
+
+    @Lazy
+    @Autowired
+    private StageManager stageManager;
 
     @FXML
     public void minimize(ActionEvent event) {
@@ -32,10 +49,24 @@ public class SplashScreenController {
             xOffset = event.getSceneX();
             yOffset = event.getSceneY();
         });
-
         toolBar.setOnMouseDragged(event -> {
             stage.setX(event.getScreenX() - xOffset);
             stage.setY(event.getScreenY() - yOffset);
         });
+    }
+
+    class ShowAndWait extends Thread {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(4000);
+                Platform.runLater(() -> stageManager.rebuildStage(SignInScreenController.class));
+            } catch (InterruptedException ignored) {}
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        new ShowAndWait().start();
     }
 }
