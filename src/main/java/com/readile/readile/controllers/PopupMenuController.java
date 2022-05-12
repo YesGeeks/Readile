f233102c1bc4ab0d16e5d6358431b73b5b331a0b
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-05-12 22:19:42 +0300

    Add switch scene function to menu buttons

---
+++ src/main/java/com/readile/readile/controllers/PopupMenuController.java
@@ -0,0 +1,34 @@
+package com.readile.readile.controllers;
+
+import com.readile.readile.config.FxController;
+import com.readile.readile.views.StageManager;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@FxmlView("/fxml/PopupMenu.fxml")
+public class PopupMenuController implements FxController {
+    @Lazy
+    @Autowired
+    private StageManager stageManager;
+
+    public void viewAccount(MouseEvent mouseEvent) {
+        Parent menu = ((Node) mouseEvent.getSource()).getScene().getRoot();
+        menu.setVisible(true);
+        stageManager.rebuildStage(AccountController.class);
+    }
+
+    public void toggleMode(MouseEvent mouseEvent) {
+    }
+
+    public void logout(MouseEvent mouseEvent) {
+        Parent menu = ((Node) mouseEvent.getSource()).getScene().getRoot();
+        menu.setVisible(true);
+        stageManager.rebuildStage(SignInScreenController.class);
+    }
+}
