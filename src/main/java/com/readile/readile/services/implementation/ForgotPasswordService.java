7744c816d3c4c0a73b317f3dbdf0b8e4040b4f5d
azzsal <azzsal@protonmail.com>
2022-05-13 21:26:23 +0300

    Add forgot password service

---
+++ src/main/java/com/readile/readile/services/implementation/ForgotPasswordService.java
@@ -0,0 +1,24 @@
+package com.readile.readile.services.implementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ForgotPasswordService {
+
+    public static final String businessEmail = "readileapp@gmail.com";
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(String receiver, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(businessEmail);
+        message.setTo(receiver);
+        message.setSubject(subject);
+        message.setText(body);
+        javaMailSender.send(message);
+    }
+}
