39a560ee0c8ab72ff226b4286eba540064a7e198
azzsal <azzsal@protonmail.com>
2022-05-17 23:05:02 +0300

    Update database schema, Add a new relationship between User_Profile and Category

--- src/main/java/com/readile/readile/models/book/Category.java
+++ src/main/java/com/readile/readile/models/book/Category.java
@@ -1,5 +1,7 @@
 package com.readile.readile.models.book;
 
+import com.readile.readile.models.user.User;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Objects;
@@ -11,26 +13,35 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    private Integer id;
+    private Long id;
 
     @Column(length = 32, nullable = false)
     private String name;
 
-    @ManyToMany(mappedBy = "categories", cascade = CascadeType.ALL)
+    @ManyToMany(mappedBy = "categories", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     Set<Book> books = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(length = 1024, name = "category_image")
+    private String categoryImage;
 
     public Category() {
     }
 
-    public Category(String name) {
+    public Category(String name, User user, String categoryImage) {
         this.name = name;
+        this.user = user;
+        this.categoryImage = categoryImage;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -40,6 +51,23 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+
+    public String getCategoryImage() {
+        return categoryImage;
+    }
+
+    public void setCategoryImage(String categoryImage) {
+        this.categoryImage = categoryImage;
     }
 
     public Set<Book> getBooks() {
@@ -59,7 +87,7 @@ public class Category {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Category category = (Category) o;
-        return Objects.equals(id, category.id) && name.equals(category.name);
+        return Objects.equals(id, category.id) && user.equals(category.user) && name.equals(category.name) && Objects.equals(categoryImage, category.categoryImage);
     }
 
     @Override
@@ -70,8 +98,11 @@ public class Category {
     @Override
     public String toString() {
         return "Category{" +
-                "category_id=" + id +
+                "id=" + id +
                 ", name='" + name + '\'' +
+                ", books=" + books +
+                ", user=" + user +
+                ", categoryImage='" + categoryImage + '\'' +
                 '}';
     }
 }
