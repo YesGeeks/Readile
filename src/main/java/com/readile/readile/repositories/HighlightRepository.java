d69821c387350105b2c4f131c350d2a4efe9b441
azzsal <azzsal@protonmail.com>
2022-05-22 11:23:29 +0300

    Update highlight repo and service

--- src/main/java/com/readile/readile/repositories/HighlightRepository.java
+++ src/main/java/com/readile/readile/repositories/HighlightRepository.java
@@ -1,8 +1,13 @@
 package com.readile.readile.repositories;
 
+import com.readile.readile.models.book.Book;
 import com.readile.readile.models.userbook.Highlight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface HighlightRepository extends JpaRepository<Highlight, Long> {}
+public interface HighlightRepository extends JpaRepository<Highlight, Long> {
+    List<Highlight> findByBook(Book book);
+}
