30ea5d51096936b7e04fbd369289301db3fbdbe7
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-08-11 00:22:39 +0300

    Fix the AccountController

--- src/main/java/com/readile/readile/controllers/user/AccountController.java
+++ src/main/java/com/readile/readile/controllers/user/AccountController.java
@@ -1,4 +1,4 @@
-package com.readile.readile.controllers;
+package com.readile.readile.controllers.user;
 
 import animatefx.animation.FadeIn;
 import com.jfoenix.controls.JFXButton;
@@ -6,14 +6,17 @@ import com.jfoenix.controls.JFXDialog;
 import com.readile.readile.config.FxController;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
-import com.readile.readile.models.book.Book;
+import com.readile.readile.controllers.PopupMenuController;
+import com.readile.readile.controllers.ToolBar;
+import com.readile.readile.controllers.authentication.SignInScreenController;
 import com.readile.readile.models.user.LoginInfo;
 import com.readile.readile.models.user.User;
-import com.readile.readile.models.userbook.UserBook;
-import com.readile.readile.services.implementation.BookService;
-import com.readile.readile.services.implementation.LoginInfoService;
-import com.readile.readile.services.implementation.UserBookService;
-import com.readile.readile.services.implementation.UserService;
+import com.readile.readile.services.implementation.authentication.LoginInfoService;
+import com.readile.readile.services.implementation.book.BookCategoryService;
+import com.readile.readile.services.implementation.book.BookService;
+import com.readile.readile.services.implementation.book.CategoryService;
+import com.readile.readile.services.implementation.book.HighlightService;
+import com.readile.readile.services.implementation.user.UserService;
 import com.readile.readile.views.Intent;
 import com.readile.readile.views.Observer;
 import com.readile.readile.views.StageManager;
@@ -41,16 +44,21 @@ import org.springframework.stereotype.Controller;
 import java.io.File;
 import java.math.BigInteger;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
 @Controller
 @FxmlView("/fxml/Account.fxml")
-public class AccountController implements Initializable, FxController, Observer {
+public class AccountController extends ToolBar implements Initializable, FxController, Observer {
+    // VIEW VARIABLES --- <
     @FXML
     public JFXButton editEmail;
     @FXML
@@ -67,39 +75,41 @@ public class AccountController implements Initializable, FxController, Observer 
     private JFXTextField username;
     @FXML
     private JFXTextField email;
-
-    @FXML
-    private HBox toolBar;
-    private double xOffset = 0, yOffset = 0;
-
     @FXML
     private JFXDialog passwordDialog;
     @FXML
     public JFXPasswordField oldPassword;
     @FXML
     public JFXPasswordField newPassword;
+    @FXML
+    private HBox toolBar;
+    // VIEW VARIABLES --- >
 
+    // SERVICES --- <
     @Lazy
     @Autowired
     StageManager stageManager;
-
     @Autowired
     UserService userService;
-
     @Autowired
     LoginInfoService loginInfoService;
-
-    @Autowired
-    UserBookService userBookService;
-
     @Autowired
     BookService bookService;
+    @Autowired
+    BookCategoryService bookCategoryService;
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    HighlightService highlightService;
+    // SERVICES --- >
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Intent.observer = this;
+        Intent.toolBar = toolBar;
+
         boolean darkTheme =  Intent.activeUser.getTheme() == 1;
-        toggleTheme(darkTheme);
+        Intent.toggleTheme(darkTheme, root);
         Intent.currentSceneClass = AccountController.class;
         fetchNavAvatar();
 
@@ -115,6 +125,12 @@ public class AccountController implements Initializable, FxController, Observer 
     @FXML
     void changePassword() {
         passwordDialog.show();
+        passwordDialog.setOnDialogClosed(
+                jfxDialogEvent -> {
+                    oldPassword.setText("");
+                    newPassword.setText("");
+                }
+        );
     }
 
     @FXML
@@ -124,7 +140,17 @@ public class AccountController implements Initializable, FxController, Observer 
             fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg"));
             File imageFile = fileChooser.showOpenDialog(((Node) event.getSource()).getScene().getWindow());
 
-            String path = "file:/" + imageFile.getPath().replace("\\", "/");
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy_HH-mm-ss");
+            Date date = new Date();
+            Path source = imageFile.toPath();
+            Path targetDir = Paths.get("data");
+
+            Files.createDirectories(targetDir); //in case target directory didn't exist
+
+            Path target = targetDir.resolve(formatter.format(date) + imageFile.getName().substring(imageFile.getName().lastIndexOf('.')));
+            Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+
+            String path = ("file:/" + target.toAbsolutePath()).replace("\\", "/");
             Image profile = new Image(path);
             profileImage.setFill(new ImagePattern(profile));
             Intent.activeUser.setProfileImage(path);
@@ -146,14 +172,12 @@ public class AccountController implements Initializable, FxController, Observer 
 
     @FXML
     void deleteAccount() {
-        List<UserBook> list = userBookService.findAllByUser(Intent.activeUser);
-        userBookService.deleteInBatch(list);
         loginInfoService.delete(loginInfoService.findByUser(Intent.activeUser));
+        categoryService.findByUser(Intent.activeUser).forEach(category -> bookCategoryService.deleteInBatch(bookCategoryService.findAllByCategory(category)));
+        categoryService.deleteInBatch(categoryService.findByUser(Intent.activeUser));
+        bookService.findAllByUser(Intent.activeUser).forEach(book -> highlightService.deleteInBatch(highlightService.findByBook(book)));
+        bookService.deleteInBatch(bookService.findAllByUser(Intent.activeUser));
         userService.delete(Intent.activeUser);
-        List<Book> books = new ArrayList<>();
-        list.forEach(userBook -> books.add(userBook.getBook()));
-        bookService.deleteInBatch(books);
-        // TODO: Delete the authors associated with the books & the user categories
         Intent.activeUser = null;
         Intent.currentSceneClass = null;
         Intent.clearBackStack();
@@ -168,11 +192,15 @@ public class AccountController implements Initializable, FxController, Observer 
         } else {
             if (validateEmailAddress(email.getText())) {
                 error.setVisible(false);
-                User newUser = Intent.activeUser;
-                newUser.setEmail(email.getText());
-                userService.update(newUser);
-                editEmail.setText("Edit");
-                email.setDisable(true);
+                try {
+                    User newUser = Intent.activeUser;
+                    newUser.setEmail(email.getText());
+                    userService.update(newUser);
+                    editEmail.setText("Edit");
+                    email.setDisable(true);
+                } catch (Exception e) {
+                    error.setVisible(true);
+                }
             } else {
                 error.setVisible(true);
             }
@@ -243,41 +271,8 @@ public class AccountController implements Initializable, FxController, Observer 
         stageManager.rebuildStage(Intent.popClosedScene());
     }
 
-    @FXML
-    void close(ActionEvent event) {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.close();
-    }
-
-    @FXML
-    void minimize(ActionEvent event) {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setIconified(true);
-    }
-
-    @FXML
-    void move(MouseEvent mouseEvent) {
-        Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
-
-        toolBar.setOnMousePressed(event -> {
-            xOffset = event.getSceneX();
-            yOffset = event.getSceneY();
-        });
-        toolBar.setOnMouseDragged(event -> {
-            stage.setX(event.getScreenX() - xOffset);
-            stage.setY(event.getScreenY() - yOffset);
-        });
-    }
-
-    public void toggleTheme(boolean isDarkTheme) {
-        if(isDarkTheme)
-            root.getStyleClass().add("dark-theme");
-        else
-            root.getStyleClass().remove("dark-theme");
-    }
-
     @Override
     public void notification(boolean isDarkTheme) {
-        toggleTheme(isDarkTheme);
+        Intent.toggleTheme(isDarkTheme, root);
     }
 }
