1058b7ea23f364fbad26234ac18a990f7d6d6d72
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-08-11 12:05:17 +0300

    Add Lombok annotations

--- src/main/java/com/readile/readile/utils/ResultBook.java
+++ src/main/java/com/readile/readile/utils/ResultBook.java
@@ -1,62 +1,17 @@
 package com.readile.readile.utils;
 
+import lombok.*;
+
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class ResultBook {
     private String name;
     private String coverURL;
     private Integer length;
     private List<String> authorNames;
-
-    public ResultBook() {
-    }
-
-    public ResultBook(String name, String coverURL, Integer length, List<String> authorNames) {
-        this.name = name;
-        this.coverURL = coverURL;
-        this.length = length;
-        this.authorNames = authorNames;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCoverURL() {
-        return coverURL;
-    }
-
-    public void setCoverURL(String coverURL) {
-        this.coverURL = coverURL;
-    }
-
-    public Integer getLength() {
-        return length;
-    }
-
-    public void setLength(Integer length) {
-        this.length = length;
-    }
-
-    public List<String> getAuthorNames() {
-        return authorNames;
-    }
-
-    public void setAuthorNames(List<String> authorNames) {
-        this.authorNames = authorNames;
-    }
-
-    @Override
-    public String toString() {
-        return "ResultBook{" +
-                "name='" + name + '\'' +
-                ", coverURL='" + coverURL + '\'' +
-                ", length=" + length +
-                ", authorNames=" + authorNames +
-                '}';
-    }
 }
