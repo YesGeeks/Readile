da0498b6e60649259e577a214f0828453a4455ac
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-08-11 11:37:50 +0300

    Create ToolBar utility class

---
+++ src/main/java/com/readile/readile/controllers/ToolBar.java
@@ -0,0 +1,38 @@
+package com.readile.readile.controllers;
+
+import com.readile.readile.views.Intent;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class ToolBar {
+    private double xOffset = 0, yOffset = 0;
+
+    @FXML
+    public void minimize(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+    @FXML
+    public void close(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    public void move(MouseEvent mouseEvent) {
+        Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+
+        Intent.toolBar.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        Intent.toolBar.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+    }
+}
