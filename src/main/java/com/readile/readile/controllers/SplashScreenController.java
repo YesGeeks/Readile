a3715ff8f8dccb3238cbe650d2f964a76bcbf423
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-08-11 12:06:19 +0300

    Reformat the code

--- src/main/java/com/readile/readile/controllers/SplashScreenController.java
+++ src/main/java/com/readile/readile/controllers/SplashScreenController.java
@@ -2,17 +2,14 @@ package com.readile.readile.controllers;
 
 import com.jthemedetecor.OsThemeDetector;
 import com.readile.readile.config.FxController;
+import com.readile.readile.controllers.authentication.SignInScreenController;
 import com.readile.readile.views.Intent;
 import com.readile.readile.views.StageManager;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -23,43 +20,19 @@ import java.util.ResourceBundle;
 
 @Controller
 @FxmlView("/fxml/Splash.fxml")
-public class SplashScreenController implements Initializable, FxController {
+public class SplashScreenController extends ToolBar implements Initializable, FxController {
+    // VIEW VARIABLES --- <
     @FXML
     private AnchorPane root;
-
     @FXML
     public HBox toolBar;
-    private double xOffset = 0, yOffset = 0;
+    // VIEW VARIABLES --- >
 
+    // SERVICES --- <
     @Lazy
     @Autowired
-    private StageManager stageManager;
-
-    @FXML
-    public void minimize(ActionEvent event) {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setIconified(true);
-    }
-
-    @FXML
-    public void close(ActionEvent event) {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.close();
-    }
-
-    @FXML
-    public void move(MouseEvent mouseEvent) {
-        Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
-
-        toolBar.setOnMousePressed(event -> {
-            xOffset = event.getSceneX();
-            yOffset = event.getSceneY();
-        });
-        toolBar.setOnMouseDragged(event -> {
-            stage.setX(event.getScreenX() - xOffset);
-            stage.setY(event.getScreenY() - yOffset);
-        });
-    }
+    StageManager stageManager;
+    // SERVICES --- >
 
     class ShowAndWait extends Thread {
         @Override
@@ -73,19 +46,13 @@ public class SplashScreenController implements Initializable, FxController {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Intent.toolBar = toolBar;
+
         final OsThemeDetector detector = OsThemeDetector.getDetector();
         final boolean isDarkThemeUsed = detector.isDark();
-        toggleTheme(isDarkThemeUsed);
+        Intent.toggleTheme(isDarkThemeUsed, root);
         Intent.clearBackStack();
-
-        detector.registerListener(isDarkTheme -> Platform.runLater(() -> toggleTheme(isDarkTheme)));
+        detector.registerListener(isDarkTheme -> Platform.runLater(() -> Intent.toggleTheme(isDarkThemeUsed, root)));
         new ShowAndWait().start();
-    }
-
-    public void toggleTheme(boolean isDarkTheme) {
-        if (isDarkTheme)
-            root.getStyleClass().add("dark-theme");
-        else
-            root.getStyleClass().remove("dark-theme");
     }
 }
