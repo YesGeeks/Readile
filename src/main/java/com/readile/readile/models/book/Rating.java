a0bd9cb03b993978b1d7ee82b8ec785ae4cd1e28
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-08-11 11:39:41 +0300

    Move to models/book

--- src/main/java/com/readile/readile/models/book/Rating.java
+++ src/main/java/com/readile/readile/models/book/Rating.java
@@ -1,4 +1,4 @@
-package com.readile.readile.models.userbook;
+package com.readile.readile.models.book;
 
 public enum Rating {
     ONE_STAR, TWO_STARS, THREE_STARS, FOUR_STARS, FIVE_STARS
--- src/main/java/com/readile/readile/models/book/Status.java
+++ src/main/java/com/readile/readile/models/book/Status.java
@@ -1,4 +1,4 @@
-package com.readile.readile.models.userbook;
+package com.readile.readile.models.book;
 
 public enum Status {
     TO_READ, CURRENTLY_READING, READ
