3735a8b288560682cf16fdea0a8e7cc85cc75498
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-08-11 12:04:00 +0300

    Reformat the code

--- src/main/java/com/readile/readile/controllers/PopupMenuController.java
+++ src/main/java/com/readile/readile/controllers/PopupMenuController.java
@@ -1,8 +1,10 @@
 package com.readile.readile.controllers;
 
 import com.readile.readile.config.FxController;
+import com.readile.readile.controllers.authentication.SignInScreenController;
+import com.readile.readile.controllers.user.AccountController;
 import com.readile.readile.models.user.User;
-import com.readile.readile.services.implementation.UserService;
+import com.readile.readile.services.implementation.user.UserService;
 import com.readile.readile.views.Observer;
 import com.readile.readile.views.Subject;
 import com.readile.readile.views.Intent;
@@ -13,6 +15,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.web.WebEngine;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -26,17 +29,20 @@ import java.util.ResourceBundle;
 @Controller
 @FxmlView("/fxml/PopupMenu.fxml")
 public class PopupMenuController implements FxController, Initializable, Subject {
+    // VIEW VARIABLES --- <
     @FXML
     private Pane root;
+    // VIEW VARIABLES --- >
 
     protected List<Observer> observers = new ArrayList<>();
 
+    // SERVICES --- <
     @Lazy
     @Autowired
-    private StageManager stageManager;
-
+    StageManager stageManager;
     @Autowired
-    private UserService userService;
+    UserService userService;
+    // SERVICES --- >
 
     public void viewAccount(MouseEvent mouseEvent) {
         Parent menu = ((Node) mouseEvent.getSource()).getScene().getRoot();
@@ -56,26 +62,25 @@ public class PopupMenuController implements FxController, Initializable, Subject
     }
 
     public void logout(MouseEvent mouseEvent) {
+        if (Intent.activeUser.getRegistration().equals("GOOGLE")) {
+            WebEngine webEngine = new WebEngine();
+            String logoutRedirectUrl = "https://accounts.google.com/Logout";
+            webEngine.load(logoutRedirectUrl);
+        }
         Parent menu = ((Node) mouseEvent.getSource()).getScene().getRoot();
         menu.setVisible(true);
         Intent.clearBackStack();
         Intent.activeUser = null;
         Intent.currentSceneClass = null;
+        Intent.isLogin = false;
         stageManager.rebuildStage(SignInScreenController.class);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         boolean darkTheme = Intent.activeUser.getTheme() == 1;
-        toggleTheme(darkTheme);
+        Intent.toggleTheme(darkTheme, root);
         this.addSubscriber(Intent.observer);
-    }
-
-    public void toggleTheme(boolean isDarkTheme) {
-        if (isDarkTheme)
-            root.getStyleClass().add("dark-theme");
-        else
-            root.getStyleClass().remove("dark-theme");
     }
 
     @Override
