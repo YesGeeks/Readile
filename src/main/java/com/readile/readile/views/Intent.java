09b21a865feba7015d67e52e3a97c7901a982e45
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-08-11 12:09:33 +0300

    Add toggleTheme method and reformat the code

--- src/main/java/com/readile/readile/views/Intent.java
+++ src/main/java/com/readile/readile/views/Intent.java
@@ -3,8 +3,9 @@ package com.readile.readile.views;
 import com.jfoenix.controls.JFXDialog;
 import com.readile.readile.config.FxController;
 import com.readile.readile.models.user.User;
-import com.readile.readile.models.userbook.UserBookId;
 import com.readile.readile.utils.ResultBook;
+import javafx.scene.Parent;
+import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
 
 import java.util.ArrayList;
@@ -16,9 +17,18 @@ public class Intent {
     public static JFXDialog addNewBookDialog;
     public static Class<? extends FxController> currentSceneClass;
     public static Observer observer;
-
     private static final Stack<Class<? extends FxController>> backStack = new Stack<>();
     public static Circle innerCircle;
+    public static HBox toolBar;
+    public static Long bookId;
+    public static boolean isLogin;
+    public static String bookName;
+    public static String freq;
+    public static int count;
+    public static List<ResultBook> tempSearchResults = new ArrayList<>();
+    public static Long categoryId;
+    public static int generatedSecurityCode;
+    public static long sendingTime;
 
     public static void pushClosedScene(Class<? extends FxController> sceneController) {
         backStack.push(sceneController);
@@ -32,8 +42,6 @@ public class Intent {
         return backStack.pop();
     }
 
-    public static List<ResultBook> tempSearchResults = new ArrayList<>();
-
     public static void set(List<ResultBook> resultBookList) {
         tempSearchResults.clear();
         tempSearchResults.addAll(resultBookList);
@@ -43,10 +51,10 @@ public class Intent {
         tempSearchResults.clear();
     }
 
-    public static Long categoryId;
-
-    public static UserBookId userBookId;
-
-    public static int generatedSecurityCode;
-    public static long sendingTime;
+    public static void toggleTheme(boolean isDarkTheme, Parent root) {
+        if (isDarkTheme)
+            root.getStyleClass().add("dark-theme");
+        else
+            root.getStyleClass().remove("dark-theme");
+    }
 }
