f2a7ce99d330f2326e821e46be55291a22f61a37
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-08-11 11:24:08 +0300

    Add BookCategory utility

--- src/main/java/com/readile/readile/repositories/BookCategoryRepository.java
+++ src/main/java/com/readile/readile/repositories/BookCategoryRepository.java
@@ -1,9 +1,8 @@
 package com.readile.readile.repositories;
 
-import com.readile.readile.models.book.AuthorBook;
 import com.readile.readile.models.book.Book;
-import com.readile.readile.models.book.BookCategory;
-import com.readile.readile.models.book.Category;
+import com.readile.readile.models.book.category.BookCategory;
+import com.readile.readile.models.book.category.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,10 +10,7 @@ import java.util.List;
 
 @Repository
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
-
     List<BookCategory> findAllByCategory(Category category);
 
     List<BookCategory> findAllByBook(Book book);
-
-    void deleteAllByBook(Book book);
 }
---
+++ src/main/java/com/readile/readile/services/implementation/book/BookCategoryService.java
@@ -0,0 +1,60 @@
+package com.readile.readile.services.implementation.book;
+
+import com.readile.readile.models.book.Book;
+import com.readile.readile.models.book.category.BookCategory;
+import com.readile.readile.models.book.category.Category;
+import com.readile.readile.repositories.BookCategoryRepository;
+import com.readile.readile.services.CrudService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookCategoryService implements CrudService<BookCategory> {
+    @Autowired
+    BookCategoryRepository bookCategoryRepository;
+
+    @Override
+    public BookCategory save(BookCategory entity) {
+        return bookCategoryRepository.save(entity);
+    }
+
+    @Override
+    public BookCategory update(BookCategory entity) {
+        return bookCategoryRepository.save(entity);
+    }
+
+    @Override
+    public void delete(BookCategory entity) {
+        bookCategoryRepository.delete(entity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bookCategoryRepository.deleteById(id);
+    }
+
+    @Override
+    public void deleteInBatch(List<BookCategory> entities) {
+        bookCategoryRepository.deleteAllInBatch(entities);
+    }
+
+    @Override
+    public BookCategory findById(Long id) {
+        return bookCategoryRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<BookCategory> findAll() {
+        return bookCategoryRepository.findAll();
+    }
+
+    public List<BookCategory> findAllByCategory(Category category) {
+        return bookCategoryRepository.findAllByCategory(category);
+    }
+
+    public List<BookCategory> findAllByBook(Book book) {
+        return bookCategoryRepository.findAllByBook(book);
+    }
+}
